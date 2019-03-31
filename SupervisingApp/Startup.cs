375b62c1b770605b741da18622d19aff6fabd271
asus <asus@ANTER>
2019-03-31 23:41:37 +0200

    comitanter

--- SupervisingApp/Startup.cs
+++ SupervisingApp/Startup.cs
@@ -14,7 +14,7 @@ namespace NajmDefault
             ConfigureAuth(app);
             app.MapSignalR();
             createRolesandUsers();
-            // test mod test
+            // test mod test 1
         }
 
         // In this method we will create default User roles and Admin user for login   
